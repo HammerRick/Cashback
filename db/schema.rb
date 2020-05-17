9a0bea1fc8e8bfa135e29eae15a3c58fffe6c3d7
emerick94 <lucas.emerick94>
2020-05-17 20:54:21 -0300

    add login options and simple navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_185119) do
+ActiveRecord::Schema.define(version: 2020_05_17_210455) do
 
   create_table "offers", force: :cascade do |t|
     t.string "advertiser_name", null: false
@@ -34,8 +34,11 @@ ActiveRecord::Schema.define(version: 2020_05_17_185119) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
