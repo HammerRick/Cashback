f0a56fa76dca78a4d461ae00a432cbcf93d2a434
emerick94 <lucas.emerick94>
2020-05-17 18:18:53 -0300

    re scaffolds user with password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
   has_and_belongs_to_many :offers
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
 end
