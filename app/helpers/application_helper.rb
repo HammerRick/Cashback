0257b010e572e14f598e5e1db26ea0e623a90884
emerick94 <lucas.emerick94>
2020-05-18 01:14:03 -0300

    fix for assets in production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def embedded_svg(filename, options = {})
-    assets = Rails.application.assets
+    assets = Rails.application.assets || Rails.application.config.assets
     file = assets.find_asset(filename).source.force_encoding("UTF-8")
     doc = Nokogiri::HTML::DocumentFragment.parse file
     svg = doc.at_css "svg"
@@ -21,7 +21,7 @@ module ApplicationHelper
     else "alert-#{flash_type}"
     end
   end
-  
+
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
       concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}", role: "alert") do 
